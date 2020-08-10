31fa1c3977fa9ef3f29343a8f877fd431436f1db
iPavelGeorgiev <iPavelGeorgiev@outlook.com>
2020-08-10 17:02:07 +0300

    feat: implement footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Footer = () => {
+   return (
+      <footer>
+         <div className={styles.footer}>
+            <p>@Palevry : Designed by <span>Pavel Genadiev Georgiev</span></p>
+         </div>
+      </footer>
+   )
+}
+
+export default Footer;
