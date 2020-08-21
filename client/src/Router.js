5237ff4b3b3cc6bc827c46d9815635e5a5243444
iPavelGeorgiev <iPavelGeorgiev@outlook.com>
2020-08-21 23:37:46 +0300

    feat: implement restaurant menu page

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "./pages/Main";
 import AboutUs from "./pages/AboutUs";
+import Menu from "./pages/Menu";
 
 import {
    BrowserRouter,
@@ -15,6 +16,7 @@ import {
          <Switch>
            <Route path="/" exact component={Main} />
            <Route path="/about" component={AboutUs} />
+           <Route path="/menu" component={Menu} />
          </Switch>
        </BrowserRouter>
    );
